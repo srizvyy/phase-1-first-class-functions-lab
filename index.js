4cb4246e7ded0b7e56d91ddcef53064d6bfa748b
Saim Rizvi <shr2mz@gmail.com>
2021-09-26 01:42:50 -0400

    commit tree 4db1c90b6dcbf233c70ef82b37b9b4c44d76404e

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia']
+
+const returnFirstTwoDrivers = function () {
+    return drivers.slice(0,2)
+}
+
+const returnLastTwoDrivers = () => drivers.slice(2)
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function (fare) {
+    const multiplier = function () {
+        return fare * 2
+        
+    }
+    fare()
+    return multiplier
+    
+}
